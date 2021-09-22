8c6810d6ecaecc9821f3e8a3663e8d782c6e425d
BogdanShutko <bogdanshutko4job@gmail.com>
2021-09-22 03:25:39 +0300

    Made some new features (ContextAPI)
    It was so difficult

--- pokemon-game/src/App.js
+++ pokemon-game/src/App.js
@@ -1,44 +1,50 @@
 import HomePage from "./routes/Home";
 import GamePage from "./routes/Game";
-import { useRouteMatch, Route, Switch, Redirect } from "react-router-dom";
+import { useLocation, Route, Switch, Redirect } from "react-router-dom";
 import About from "./routes/about/about";
 import Contact from "./routes/Contact/contact";
 import MenuHeader from "./routes/MenuHeader/MenuHeader";
 import Footer from "./components/Footer/Footer";
 import NotFound from "./routes/NotFound/NotFound";
+import { FirebaseContext } from "./components/context/firebaseContext";
 import s from "./style.module.css";
 import cn from "classnames";
+import Firebase from "./service/firebase";
 
 const App = () => {
-  const match = useRouteMatch("/");
+  const location = useLocation();
+  const isPadding =
+    location.pathname === "/" || location.pathname === "/game/board";
   return (
-    <Switch>
-      <Route path="/404" component={NotFound} />
-      <Route>
-        <>
-          <MenuHeader bgActive={!match.isExact} />
-          <div
-            className={cn(s.wrap, {
-              [s.isHomePage]: match.isExact,
-            })}
-          >
-            <Switch>
-              <Route path="/" exact component={HomePage}></Route>
-              <Route path="/home" component={HomePage} />
-              <Route path="/game" component={GamePage}></Route>
-              <Route path="/about" component={About}></Route>
-              <Route path="/contact" component={Contact}></Route>
-              <Route
-                render={() => {
-                  return <Redirect to="/404" />;
-                }}
-              />
-            </Switch>
-          </div>
-          <Footer />
-        </>
-      </Route>
-    </Switch>
+    <FirebaseContext.Provider value={new Firebase()}>
+      <Switch>
+        <Route path="/404" component={NotFound} />
+        <Route>
+          <>
+            <MenuHeader bgActive={!isPadding} />
+            <div
+              className={cn(s.wrap, {
+                [s.isHomePage]: isPadding,
+              })}
+            >
+              <Switch>
+                <Route path="/" exact component={HomePage}></Route>
+                <Route path="/home" component={HomePage} />
+                <Route path="/game" component={GamePage}></Route>
+                <Route path="/about" component={About}></Route>
+                <Route path="/contact" component={Contact}></Route>
+                <Route
+                  render={() => {
+                    return <Redirect to="/404" />;
+                  }}
+                />
+              </Switch>
+            </div>
+            <Footer />
+          </>
+        </Route>
+      </Switch>
+    </FirebaseContext.Provider>
   );
 };
 
